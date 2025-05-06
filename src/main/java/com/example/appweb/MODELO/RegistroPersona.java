a18c3567b89b821c98a2d370499be9669531efb6
BenjaminG17 <benjamin.gonzalez.f@alumnos.upla.cl>
2025-05-06 00:04:21 -0400

    .
    Cambios en registroPersona

--- src/main/java/com/example/appweb/MODELO/RegistroPersona.java
+++ src/main/java/com/example/appweb/MODELO/RegistroPersona.java
@@ -1,18 +1,24 @@
 package com.example.appweb.MODELO;
 
+import java.sql.Date;
+
 public class RegistroPersona {
     private String rut;
     private String nombre;
+    private Date fechaHora;
     private String tipoRegistro;
 
     public RegistroPersona() {}
 
-    public RegistroPersona(String rut, String nombre, String tipoRegistro) {
+    // Constructor completo
+    public RegistroPersona(String rut, String nombre, Date fechaHora, String tipoRegistro) {
         this.rut = rut;
         this.nombre = nombre;
+        this.fechaHora = fechaHora;
         this.tipoRegistro = tipoRegistro;
     }
 
+    // Getters y setters
     public String getRut() {
         return rut;
     }
@@ -27,6 +33,14 @@ public class RegistroPersona {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
     }
 
     public String getTipoRegistro() {
