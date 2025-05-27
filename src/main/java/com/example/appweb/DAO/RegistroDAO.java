f53f4c9023513d3837ec6b36b1c53cfa49aedc54
BenjaminG17 <benjamin.gonzalez.f@alumnos.upla.cl>
2025-05-27 18:40:34 -0400

    .

--- src/main/java/com/example/appweb/DAO/RegistroDAO.java
+++ src/main/java/com/example/appweb/DAO/RegistroDAO.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class RegistroDAO {
