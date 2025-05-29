97c00fba4d296bf9a7111b2ea98822f90d64a4f9
TomasGonlez <tomas.gonzalez.f@alumnos.upla.cl>
2025-05-29 18:07:20 -0400

    Se arregla el formulario de crear usuario para identificar la pagina a direccionar según si hay o no una sesion activa

--- src/main/java/com/example/appweb/CONTROLADOR/UsuarioServlet.java
+++ src/main/java/com/example/appweb/CONTROLADOR/UsuarioServlet.java
@@ -11,104 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class UsuarioServlet extends HttpServlet {
 
-    /**private UsuarioDAO usuarioDAO;
-
-    @Override
-    public void init() throws ServletException{
-        usuarioDAO = new UsuarioDAO();
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String accion = request.getParameter("accion");
-
-        if ("registrar".equals(accion)) {
-            registrarUsuario(request, response);
-        } else if ("login".equals(accion)) {
-            loginUsuario(request, response);
-        } else {
-            response.sendRedirect("JSP/error.jsp");
-        }
-
-    }
-    private void registrarUsuario(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String nombreCompleto = request.getParameter("nombreCompletoUser");
-        String correo = request.getParameter("correoUser");
-        int numero = Integer.parseInt(request.getParameter("numeroUser"));
-        String nombre = request.getParameter("nombreUser");
-        String contrasena = request.getParameter("contrasena");
-
-
-        String nombreUsuario = request.getParameter("nombreUser");
-        UsuarioDAO dao = new UsuarioDAO();
-        if (dao.existeNombreUsuario(nombreUsuario)) {
-            request.setAttribute("errorNombreUSER", "El nombre de usuario ya existe.");
-            RequestDispatcher rd = request.getRequestDispatcher("JSP/crearUsuario.jsp");
-            rd.forward(request,response);
-            return;
-        }
-
-        try {
-            LocalDate fecha = LocalDate.now();
-            Usuario nuevoUsuario = new Usuario();
-            nuevoUsuario.setNombreCompletoUser(nombreCompleto);
-            nuevoUsuario.setCorreoUser(correo);
-            nuevoUsuario.setNumeroUser(numero);
-            nuevoUsuario.setNombreUser(nombre);
-            nuevoUsuario.setContrasena(contrasena);
-            nuevoUsuario.setFechaCreacion(fecha);
-
-            boolean exito = usuarioDAO.guardar(nuevoUsuario);
-
-            if (exito) {
-                // Verificar si hay sesión activa
-                HttpSession session = request.getSession(false); // false = no crear nueva sesión
-
-                if (session != null && session.getAttribute("usuarioLogueado") != null) {
-                    // Caso 1: Hay sesión -> Redirige a crearUsuario.jsp
-                    response.sendRedirect("JSP/crearUsuario.jsp");
-                } else {
-                    // Caso 2: No hay sesión -> Redirige al login con mensaje de éxito
-                    request.setAttribute("exitoRegistro", "Usuario registrado con éxito");
-                    request.getRequestDispatcher("JSP/login.jsp").forward(request, response);
-                }
-            } else {
-                response.sendRedirect("JSP/error.jsp");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.sendRedirect("JSP/error.jsp");
-        }
-    }
-    private void loginUsuario(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        try{
-            String nameUsuario = request.getParameter("nombreUsuario");
-            String contrasena = request.getParameter("contrasenaUsuario");
-            Usuario usuario = usuarioDAO.loginUsuario(nameUsuario,contrasena);
-
-            if(usuario != null){
-                //Crear una sesion o recuperar la existente
-                HttpSession session = request.getSession();
-
-                //Guardar el objeto usuario en la sesion
-                session.setAttribute("usuarioLogueado", usuario);
-
-                response.sendRedirect(request.getContextPath() + "/RegistroServlet?accion=listarRegistros");
-            }else{
-                // SI FALLA EL LOGIN, redirige al login.jsp PERO CON UN MENSAJE
-                request.setAttribute("errorLogin", "Credenciales incorrectas. Inténtalo nuevamente.");
-                request.getRequestDispatcher("JSP/login.jsp").forward(request, response);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.sendRedirect("JSP/error.jsp");
-        }
-    }**/
     private UsuarioService usuarioService;
 
     @Override
@@ -144,14 +49,6 @@ public class UsuarioServlet extends HttpServlet {
             boolean exito = usuarioService.registrarUsuario(nuevoUsuario);
 
             if (exito) {
-                /**HttpSession session = request.getSession(false);
-
-                if (session != null && session.getAttribute("usuarioLogueado") != null) {
-                    response.sendRedirect("JSP/crearUsuario.jsp");
-                } else {
-                    request.setAttribute("exitoRegistro", "Usuario registrado con éxito");
-                    request.getRequestDispatcher("JSP/login.jsp").forward(request, response);
-                }**/
                 if("SESSION".equals(origenFormulario)) {
                     response.sendRedirect("JSP/crearUsuario.jsp");
                 } else if ("NO_SESSION".equals(origenFormulario)) {
